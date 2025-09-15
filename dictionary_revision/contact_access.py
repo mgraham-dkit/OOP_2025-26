db762fcaa1a40b29373f092adbe13da75889fc8b
michelle <michelle.graham@dkit.ie>
2025-09-15 21:50:12 +0100

    (feat): Add repeating prompt to enter new contact to dictionary

--- dictionary_revision/contact_access.py
+++ dictionary_revision/contact_access.py
@@ -8,12 +8,18 @@ name = input(f"Please enter the contact's name: ")
 number = contacts.get(name)
 print(f"{name} : {number}")
 
-new_contact_name = input("Please enter the new name to be added: ")
-new_contact_num = int(input(f"Please enter {new_contact_name}'s number: "))
+finished = False
+while not finished:
+    new_contact_name = input("Please enter the new name to be added: ")
+    new_contact_num = int(input(f"Please enter {new_contact_name}'s number: "))
 
-if contacts.get(new_contact_name) is None:
-    contacts[new_contact_name] = new_contact_num
-else:
-    print("Contact already exists!")
+    if contacts.get(new_contact_name) is None:
+        contacts[new_contact_name] = new_contact_num
+        finished = True
+    else:
+        print(f"Contact already exists! {new_contact_name} : {contacts[new_contact_name]}")
+        choice = input("Do you wish to try again??? (y to continue, any other key to end)")
+        if choice != "y" and choice != "Y":
+            finished = True
 
 print(contacts)
