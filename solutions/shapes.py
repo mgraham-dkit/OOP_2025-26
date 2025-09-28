488f9187b6859f0b0e26c638bab95b4bb68304b6
michelle <michelle.graham@dkit.ie>
2025-09-29 00:12:15 +0100

    feat: Add Rectangle class
    This includes:
     - constructor for custom rectangle creation
     - display() to view a rectangle's information
     - calc_area() to calculate a rectangle's area

---
+++ solutions/shapes.py
@@ -0,0 +1,13 @@
+class Rectangle:
+    # Create a constructor that takes in the length, width and colour
+    # for each Rectangle being created
+    def __init__(self, length, width, colour):
+        self.length = length
+        self.width = width
+        self.colour = colour
+
+    def display(self):
+        print(f"Rectangle[length={self.length}, width={self.width}, colour={self.colour}]")
+
+    def calc_area(self):
+        return self.length * self.width
