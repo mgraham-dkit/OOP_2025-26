f716e9d35ce74db68c67918828d777e1494b95ec
michelle <michelle.graham@dkit.ie>
2025-09-29 13:58:23 +0100

    feat: add script to create StockItems based on user input, then search for the item with the highest profit

---
+++ intro_to_classes/stock_management.py
@@ -0,0 +1,37 @@
+from stock import StockItem
+
+
+def find_max_profit_item(stock_items):
+    max_profit = 0
+    max_item = stock_items[0]
+
+    for item in stock_items:
+        if item.calc_profit() > max_profit:
+            max_profit = item.calc_profit()
+            max_item = item
+
+    return max_item
+
+
+
+def create_stock_item():
+    code = input("Product code: ")
+    name = input("Product name: ")
+    desc = input("Product description: ")
+    cost = float(input("Cost price: "))
+    sale = float(input("Sale price: "))
+    quant = int(input("Quantity in stock: "))
+
+    stock_unit = StockItem(code, name, cost, sale, quant, desc)
+
+    return stock_unit
+
+
+stock = []
+for i in range(2):
+    unit = create_stock_item()
+    stock.append(unit)
+
+for s in stock:
+    s.display()
+    print(f"Profit: €{s.calc_profit()}")
