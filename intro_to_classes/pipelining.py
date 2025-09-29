c2448ff798665f30082cf0e38d7c3e33630f86ef
michelle <michelle.graham@dkit.ie>
2025-09-29 12:54:08 +0100

    feat: Add script creating 5 Containers based on user input

---
+++ intro_to_classes/pipelining.py
@@ -0,0 +1,22 @@
+from containers import Container
+
+def create_container():
+    print("Now creating a container!")
+    name = input("Container name: ")
+    port = int(input("Operating port: "))
+    env = input("Environment details: ")
+    username = input("Username: ")
+    password = input("Password: ")
+
+    container = Container(name, port, env, username, password)
+
+    return container
+
+
+containers = []
+for i in range(5):
+    c = create_container()
+    containers.append(c)
+
+for c in containers:
+    c.display()
