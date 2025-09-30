d73091fe837a84c4ad56b75381536d9a6198d00a
michelle <michelle.graham@dkit.ie>
2025-09-30 16:16:57 +0100

    feat: find all stock items containing specified keyword in description

--- b_intro_to_classes/stock_management.py
+++ b_intro_to_classes/stock_management.py
@@ -3,12 +3,27 @@ from stock import StockItem
 def get_prod_code(stock_item):
     return stock_item.prod_code
 
+def display_stock_list(stock):
+    print("Stock Items in system:")
+    for s in stock:
+        s.display()
+        print(f"Profit: €{s.calc_profit()}")
+    print("---------------------------")
+
 def get_by_prod_code(stock_items, prod_code):
     for item in stock_items:
         if item.prod_code == prod_code:
             return item
 
     return None
+
+def get_by_keyword(stock_items, keyword):
+    matches = []
+    for item in stock_items:
+        if keyword in item.description:
+            matches.append(item)
+
+    return matches
 
 def find_max_profit_item(stock_items):
     max_profit = 0
@@ -41,24 +56,23 @@ for i in range(2):
     unit = create_stock_item()
     stock.append(unit)
 
-print("Stock Items in system:")
-for s in stock:
-    s.display()
-    print(f"Profit: €{s.calc_profit()}")
-print("---------------------------")
+display_stock_list(stock)
 
 stock.sort(key=get_prod_code)
 
-print("Stock Items in system:")
-for s in stock:
-    s.display()
-    print(f"Profit: €{s.calc_profit()}")
-print("---------------------------")
+display_stock_list(stock)
 
 code = input("Enter the code to be searched for: ")
 match = get_by_prod_code(stock, code)
 if match is not None:
     print("Match: ")
     match.display()
+else:
+    print("No match found")
+
+keyword = input("Enter the keyword to be searched for: ")
+matches = get_by_keyword(stock, keyword)
+if len(matches) != 0:
+    display_stock_list(stock)
 else:
     print("No match found")
