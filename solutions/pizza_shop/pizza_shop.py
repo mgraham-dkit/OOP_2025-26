224a80da0c014ec4c4cf9a2c8d4c158a963a725f
michelle <michelle.graham@dkit.ie>
2025-10-14 16:34:31 +0100

    refactor: break up logic from pizza shop application into smaller functions

--- solutions/pizza_shop/pizza_shop.py
+++ solutions/pizza_shop/pizza_shop.py
@@ -1,24 +1,20 @@
 from orders import Pizza
 
 
-order = {}
-
-username = input("Enter your username: ")
-add_pizza = True
-
-while add_pizza:
-    choice = input("Do you wish to select a pizza? (Y/y for yes, any other key to close order)")
-    if choice.lower() != "y":
-        add_pizza = False
-        continue
-
+def get_nickname(user_order):
     valid_nickname = False
+    new_nickname = None
     while not valid_nickname:
-        nickname = input("Enter pizza nickname: ")
-        if nickname not in order:
+        new_nickname = input("Enter pizza nickname: ")
+        if new_nickname not in user_order:
             valid_nickname = True
         else:
             print("That nickname is in use, please try again.")
+
+    return new_nickname
+
+def get_size():
+    size = None
 
     valid_size = False
     while not valid_size:
@@ -28,8 +24,31 @@ while add_pizza:
         else:
             print("That size is invalid, please try again.")
 
-    toppings = ["tomato sauce", "cheese"]
+    return size
 
-    pizza = Pizza(toppings, size)
-    pizza.display()
-    order[nickname] = pizza
+def get_toppings():
+    return ["tomato sauce", "cheese"]
+
+def create_pizza():
+    new_size = get_size()
+    toppings = get_toppings()
+    pizza = Pizza(toppings, new_size)
+
+    return pizza
+
+
+order = {}
+
+username = input("Enter your username: ")
+
+add_pizza = True
+while add_pizza:
+    choice = input("Do you wish to select a pizza? (Y/y for yes, any other key to close order)")
+    if choice.lower() != "y":
+        add_pizza = False
+        continue
+
+    nickname = get_nickname(order)
+    new_pizza = create_pizza()
+    new_pizza.display()
+    order[nickname] = new_pizza
