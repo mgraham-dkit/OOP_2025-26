45726f5ca06197357d04238e4363b9da2f491225
michelle <michelle.graham@dkit.ie>
2025-10-20 12:33:37 +0100

    refactor: move to size price index dictionary instead of allowable size list

--- solutions/pizza_shop/orders.py
+++ solutions/pizza_shop/orders.py
@@ -1,12 +1,20 @@
 class Pizza:
-    _allowable_sizes = ["small", "medium", "large", "extra-large"]
+    #_allowable_sizes = ["small", "medium", "large", "extra-large"]
+    _SIZE_PRICE_INDEX = {
+        "small" : 10,
+        "medium" : 12,
+        "large" : 15,
+        "extra-large" : 18
+    }
+    _STANDARD_SIZE = "medium"
+    _TOPPING_COST = 0.85
 
     def __init__(self, toppings, size="medium"):
         self.toppings = toppings
-        if size.lower() in Pizza._allowable_sizes:
+        if size.lower() in Pizza._SIZE_PRICE_INDEX:
             self._size = size.lower()
         else:
-            self._size = Pizza._allowable_sizes[1]
+            self._size = Pizza._STANDARD_SIZE
 
     def display(self):
         toppings = ", ".join(self.toppings)
@@ -16,7 +24,7 @@ class Pizza:
         return self._size
 
     def set_size(self, new_size):
-        if new_size.lower() in Pizza._allowable_sizes:
+        if new_size.lower() in Pizza._SIZE_PRICE_INDEX:
             self._size = new_size.lower()
             return True
         else:
@@ -24,11 +32,11 @@ class Pizza:
 
     @staticmethod
     def get_allowable_sizes():
-        return list(Pizza._allowable_sizes)
+        return list(Pizza._SIZE_PRICE_INDEX.keys())
 
     @staticmethod
     def check_size(size):
-        if size.lower() in Pizza._allowable_sizes:
+        if size.lower() in Pizza._SIZE_PRICE_INDEX:
             return True
         else:
             return False
