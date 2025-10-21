799cccbbff632869a542d3d9162d5313f9ced5f6
michelle <michelle.graham@dkit.ie>
2025-10-21 16:24:37 +0100

    feat: add example of inheritance using stock item

--- b_intro_to_classes/stock.py
+++ b_intro_to_classes/stock.py
@@ -15,3 +15,13 @@ class StockItem:
 
     def calc_profit(self):
         return (self.sale_price - self.cost_price) * self.quantity
+
+
+class PerishableStockItem(StockItem):
+    def __init__(self, prod_code, name, expiry, origin, cost_price, sale_price, quantity, description="Default description"):
+        super().__init__(prod_code, name, cost_price, sale_price, quantity, description)
+        self.expiry = expiry
+        self.origin = origin
+
+    def display(self):
+        print(f"{self.prod_code}: {self.name} = €{self.cost_price} (cost) [Expires: {self.expiry}")
