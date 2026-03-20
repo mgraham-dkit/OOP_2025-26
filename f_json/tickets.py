04fd74367667529b2cec95a513be25c4c56ee667
michelle <michelle.graham@dkit.ie>
2026-03-20 12:24:13 +0000

    feat: basic Ticket class (str only)

--- f_json/tickets.py
+++ f_json/tickets.py
@@ -1,13 +1,10 @@
 class Ticket:
-    def __init__(self):
-        """
-            "ticket_id": int,
-            "title": str,
-            "description" : str,
-            "status" : str,
-            "assigned_to" : str
-        """
-        pass
+    def __init__(self, ticket_id: int, title: str, desc: str, status: str, assigned_to: str):
+        self.ticket_id = ticket_id
+        self.title = title
+        self.description = desc
+        self.status = status
+        self.assigned_to = assigned_to
 
     def __str__(self) -> str:
-        pass
+        return f"{self.__class__.__name__}{{ticket_id={self.ticket_id}, title={self.title}, description={self.description}, status={self.status}, assigned_to={self.assigned_to}}}"
