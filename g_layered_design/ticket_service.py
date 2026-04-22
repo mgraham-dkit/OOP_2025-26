e77a16af9154655930a16b4e75a8707c99192c66
michelle <michelle.graham@dkit.ie>
2026-04-22 22:34:05 +0100

    feat: add unassigned ticket list attribute to TicketService

--- g_layered_design/ticket_service.py
+++ g_layered_design/ticket_service.py
@@ -1,7 +1,8 @@
 from tickets import Ticket
 class TicketService:
-    def __init__(self, assigned: dict[str, list[Ticket]] = None):
+    def __init__(self, assigned: dict[str, list[Ticket]] = None, unassigned: list[Ticket] = None):
         self.__assigned_tickets = dict.copy(assigned) if assigned else {}
+        self.__unassigned_tickets = list.copy(unassigned) if unassigned else []
 
     def get_tickets_for_agent(self, agent: str):
         if agent.lower() not in self.__assigned_tickets:
