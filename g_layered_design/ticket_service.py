b9e4425cd66a594ea9d871d4cdc28b632d0cf17e
michelle <michelle.graham@dkit.ie>
2026-04-22 23:21:42 +0100

    docs: add missing return type hinting on TicketService methods

--- g_layered_design/ticket_service.py
+++ g_layered_design/ticket_service.py
@@ -9,19 +9,19 @@ class TicketService:
         self.__assigned_tickets = dict.copy(assigned) if assigned else {}
         self.__unassigned_tickets = list.copy(unassigned) if unassigned else []
 
-    def get_tickets_for_agent(self, agent: str):
+    def get_tickets_for_agent(self, agent: str) -> list[Ticket] | None:
         if agent.lower() not in self.__assigned_tickets:
             return None
 
         return  self.__assigned_tickets[agent.lower()]
 
-    def get_agents(self):
+    def get_agents(self) -> list[str]:
         return list(self.__assigned_tickets.keys())
 
     def get_unassigned_tickets(self) -> list[Ticket]:
         return list(self.__unassigned_tickets)
 
-    def _assign_ticket(self, agent: str, ticket: Ticket):
+    def _assign_ticket(self, agent: str, ticket: Ticket) -> None:
         # Link ticket to specified agent
         ticket.assign_to(agent)
 
